6c2c009063b88e55237c542988ba526fd9ebfcc8
yujian886 <yjxuexi@gmail.com>
2021-03-07 17:27:14 +0800

    implement my posts add post edit/deleted

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,25 @@ class PostsController < ApplicationController
      else
        render :new
      end
+
+     def edit
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+     end
+     def update
+       @group = Group.find(params[:group_id])
+       @post = Post.find(params[:id])
+       @post.update(post_params)
+       redirect_to account_posts_path, notice: "Update Success"
+     end
+    def destroy
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+      @post.destroy
+      flash[:alert] = "Post Delted"
+      redirect_to account_posts_path
+    end
+
    end
 
 
